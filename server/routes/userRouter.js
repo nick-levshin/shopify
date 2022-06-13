d631d8c7272a0b38813cbc7b00cf820fc169e09e
Nick Levshin <nikita.levshin2016@gmail.com>
2022-06-13 23:38:38 +0300

    feat: add add registration and login

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,19 @@
 const Router = require('express');
 const userController = require('../controllers/userController');
+const { check } = require('express-validator');
 const router = new Router();
 
-router.post('/registration', userController.registration);
+router.post(
+  '/registration',
+  [
+    check('email', 'Incorrect email').isEmail(),
+    check(
+      'password',
+      'Password must be longer than 6 and shorter than 12'
+    ).isLength({ min: 6, max: 12 }),
+  ],
+  userController.registration
+);
 router.post('/login', userController.login);
 router.get('/auth', userController.check);
 
