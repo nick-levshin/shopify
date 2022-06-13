526508f4a4bb3aaf8a66d630ee5fe36b9d3cfd10
Nick Levshin <nikita.levshin2016@gmail.com>
2022-06-13 19:39:53 +0300

    feat: add routes

---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+class UserController {
+  async registration(req, res) {}
+
+  async login(req, res) {}
+
+  async check(req, res) {
+    const { id } = req.query;
+    res.json(query);
+  }
+}
+
+module.exports = new UserController();
